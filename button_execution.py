2fa6b73baa5f10b3cdf00f3f5e89e5be89fbbdaf
anjaligorti <54911406+anjaligorti@users.noreply.github.com>
2025-09-28 15:24:42 -0500

    structure repo

---
+++ button_execution.py
@@ -0,0 +1,17 @@
+"""
+Movement executions:
+
+- Power on/E-stop/power off
+- Teach mode (saving positions)
+    - first press = tech mode one
+    - Second press = save in front of mouth position
+    - Third press = save outside of mouth
+- Utensil select
+    - Press to select utensil + bring to mouth?
+    - 2nd press to put utensil back and detach
+- Feeding (bite button)
+    - Choose utensil and attach to arm, press to go to mouth position
+    - While button pressed, push food into mouth
+- Double click to “undo”/put bite back?
+
+"""
---
+++ utils/motor_control.py
@@ -0,0 +1,8 @@
+"""
+Motion profiling for motors
+
+- Localize motors
+- PID for motors to and from specific position
+- Planning for multimotor movements
+
+"""
